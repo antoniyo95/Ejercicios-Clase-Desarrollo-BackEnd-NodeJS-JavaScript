79a0b7ef336c066d02447584f35239086428cde3
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-02-10 13:53:41 +0100

    Subo al repositorio lo cursado en el tercer dia

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -3,7 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+res.locals.nombre = 'Antonio';
+
+res.locals.usuarios = [
+  {nombre: 'Smith', edad: 34},
+  {nombre: 'Jones', edad: 23}
+];
+
+const ahora = new Date();
+res.locals.paridad = ahora.getSeconds() % 2 === 0;
+res.locals.segundoActual = ahora.getSeconds();
+
+  //res.render('index', { title: 'NodeApp' });
+  res.render('index');
 });
+
+router.get('/parametro_en_ruta/:numero', (req, res, next) => {
+  console.log(req.params);
+  const numero = req.params.numero;
+
+  res.send('Me has pedido el numero ' + numero);
+})
+
+router.get('/parametro_opcional/:numero?', (req, res, next) => {
+  console.log(req.params);
+  const numero = req.params.numero;
+
+  res.send('(Opcional) Me has pedido el numero ' + numero);
+})
+
+router.get('/producto/:nombre/talla/:talla([0-9]+)/color/:color', (req, res, next) => {
+  const nombre = req.params.nombre;
+  const talla= req.params.talla;
+  const color = req.params.color;
+
+  res.send(`Me pediste ${nombre} ${talla} ${color}`);
+
+})
 
 module.exports = router;
